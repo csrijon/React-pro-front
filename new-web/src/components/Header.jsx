9388de22ff42df37741eb2d8b5aa57c4b5919dde
csrijon <csrijon92@gmail.com>
2025-10-25 23:40:02 +0530

    push

--- new-web/src/components/Header.jsx
+++ new-web/src/components/Header.jsx
@@ -30,9 +30,9 @@ const Header = () => {
       {/* Mobile Navigation */}
       <nav className={`mobile-nav ${isOpen ? 'is-open' : 'is-off'}`}>
         <ul className="nav-links">
-          <li><span>#</span><a href="">home</a></li>
+          <li><span>#</span><a  href="">home</a></li>
           <li><span>#</span><a href="">works</a></li>
-          <li><span>#</span><a href="">about-me</a></li>
+          <li><span>#</span><a onClick={()=>setIsOpen(false)}  href="#about">about-me</a></li>
           <li><span>#</span><a href="">contact</a></li>
           <li><span>#</span><a href="">chat-me</a></li>
         </ul>
