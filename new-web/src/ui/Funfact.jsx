5e69537fad55231ffe924bad41a4dabeb730b157
csrijon <csrijon92@gmail.com>
2025-10-28 13:37:34 +0530

    push

--- new-web/src/ui/Funfact.jsx
+++ new-web/src/ui/Funfact.jsx
@@ -4,16 +4,16 @@ import './Fanfact.css'
 const Funfact = () => {
 
     const data = [
-        { fact: "I love hiking and exploring new trails." },
-        { fact: "I can code for hours if there’s coffee involved." },
-        { fact: "I once built a website entirely on my phone during a train ride." },
-        { fact: "My favorite debugging tool is a cup of chai and some lo-fi music." },
-        { fact: "I enjoy solving logic puzzles and brain teasers." },
-        { fact: "I can remember CSS color codes better than phone numbers." },
-        { fact: "I’m a night owl developer — most of my best ideas come after midnight." },
-        { fact: "I once spent 3 hours fixing a bug that needed just one missing semicolon." },
-        { fact: "I enjoy learning random tech facts just for fun." },
-        { fact: "I love clean, minimal, and responsive UI design." }
+        { fact: "I love hiking and exploring new trail" },
+        { fact: "I can code for hours if there’s" },
+        { fact: "I once built a website entirel" },
+        { fact: "My favorite debugging tool is a" },
+        { fact: "I enjoy solving logic puzzles" },
+        { fact: "I can remember CSS color codes" },
+        { fact: "I’m a night owl developer — most of" },
+        { fact: "I once spent 3 hours fixing a bug" },
+        { fact: "I enjoy learning random tech facts" },
+        { fact: "I love clean, minimal, and" }
     ];
 
     return (
