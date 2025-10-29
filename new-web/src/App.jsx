5a8cbca58f96803ad59105d6e914cf0cf14d5483
csrijon <csrijon92@gmail.com>
2025-10-29 22:32:05 +0530

    push

--- new-web/src/App.jsx
+++ new-web/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
-// import Quote from './components/Quote'
 import ProjectSection from './components/ProjectSection'
 import Skillsection from './components/Skillsection'
 import AboutMe from './components/AboutMe'
