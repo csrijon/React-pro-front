661a14337df640ef6aebba04ffc8ab1198bbb620
csrijon <csrijon92@gmail.com>
2025-11-03 22:36:25 +0530

    push

--- new-web/src/ui/Aboutmeimage.jsx
+++ new-web/src/ui/Aboutmeimage.jsx
@@ -4,7 +4,7 @@ const Aboutmeimage = () => {
     return (
         <div className="about-me-image-section">
             <div className="dots dots-top"></div>
-            <img src={profileImage} alt="Elias" className="profile-image" />
+            <img src={profileImage} alt="srijon" className="profile-image" />
             <div className="dots dots-bottom"></div>
         </div>
     )
