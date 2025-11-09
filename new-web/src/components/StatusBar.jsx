c8e0c5f8ac81aec4bdf6a66462f011f006f6866c
csrijon <csrijon92@gmail.com>
2025-11-10 02:02:26 +0530

    push

--- new-web/src/components/StatusBar.jsx
+++ new-web/src/components/StatusBar.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import "./StatusBar.css"; 
+import "./StatusBar.css";
+import { motion } from "framer-motion";
 
 const StatusBar = () => {
   return (
-    <div className="status-bar">
-      <span className="status-icon"></span>
-      <span className="status-text">
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0, backgroundColor: "#1f2227" }}
+      transition={{ duration: 0.5, ease: "easeInOut", delay: 1 }}
+      className="status-bar">
+      <motion.span
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5, ease: "easeInOut", delay: 1.3 }}
+        className="status-icon"></motion.span>
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeInOut", delay: 1.5 }}
+        className="status-text">
         Currently working on <strong>Portfolio</strong>
-      </span>
-    </div>
+      </motion.span>
+    </motion.div>
   );
 };
 
