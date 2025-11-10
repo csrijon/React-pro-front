3bbea4c295f7d3deda64411a7fe11db423ae34dd
csrijon <csrijon92@gmail.com>
2025-11-10 18:47:59 +0530

    push

--- new-web/src/components/Skillsection.jsx
+++ new-web/src/components/Skillsection.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import "./Skill.css"
-// import skills from "../assets/skills.webp"
+import {motion} from "framer-motion"
 import SkillsSection from './SkillsSection'
 
 const Skillsection = () => {
     return (
         <div className='skill-section' >
             {/* <div className=""> */}
-            <h2 className="top-section">
+            <motion.h2 
+            initial={{opacity:0,x:-100,scale:0}}
+            whileInView={{opacity:1,x:0,scale:1.1}}
+            transition={{duration:0.6,ease:"easeInOut"}}
+            className="top-section">
                 <span className="accent-text">#</span>skills
-            </h2>
+            </motion.h2>
 
             {/* </div> */}
             <div className="middle-skill">
