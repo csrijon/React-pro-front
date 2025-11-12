24bd5247793a3d58553ba04a7c2e387eb775d26a
csrijon <csrijon92@gmail.com>
2025-11-13 01:27:34 +0530

    push

--- new-web/src/ui/Aboutme.jsx
+++ new-web/src/ui/Aboutme.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import './Aboutpart.css';
+import { motion } from "framer-motion";
 
 const Aboutme = () => {
     return (
         <div className="about-me-text">
-            <p>Hello, I'm Srijon!</p>
-            <p>
+            <motion.p 
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{opacity:1,y:0}}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+            >Hello, I'm Srijon!</motion.p>
+            <motion.p 
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{opacity:1,y:0}}
+            transition={{ duration: 0.6, ease: "easeOut",delay:0.32 }}
+            >
                 a passionate developer who has completed my BCA from Maulana Abul Kalam Azad University of Technology and I am currently pursuing MCA at JIS College of Engineering, Kalyani. I specialize in building full-stack web applications and mobile apps, focusing on clean UI and smooth user experience.
-            </p>
-            <p>
+            </motion.p>
+            <motion.p 
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{opacity:1,y:0}}
+            transition={{ duration: 0.6, ease: "easeOut",delay:0.64 }}
+            >
                 Along with development, I am currently learning Data Structures and Algorithms (DSA) using C++ to strengthen my problem-solving and technical depth.
-            </p>
+            </motion.p>
         </div>
     )
 }
