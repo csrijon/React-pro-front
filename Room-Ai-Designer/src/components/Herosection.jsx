284a9ee1947bcbaede7e50ee7ffb83f5892ca53e
csrijon <csrijon92@gmail.com>
2025-11-18 15:46:23 +0530

    push

---
+++ Room-Ai-Designer/src/components/Herosection.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../css/Herosection.css";
+import heroImage from "../assets/heroimage.png";
+
+const Herosection = () => {
+
+    
+    return (
+        <section className="hero" >
+            <div className="hero-container">
+                <div className="hero-left">
+                    <h1>Unlock your Dream Space with AI</h1>
+                    <p>
+                        Transform mobile scans into stunning 3D interiors,
+                        visualize in AR/VR.
+                    </p>
+                </div>
+                <div className="hero-right">
+                    <img src={heroImage} alt="3dimage" />
+                </div>
+            </div>           
+        </section>
+    )
+}
+export default Herosection;
