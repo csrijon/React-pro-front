143111c7c1fcdb7dee86e8e696c9ba43be01581c
csrijon <csrijon92@gmail.com>
2025-11-19 15:49:24 +0530

    push

--- Room-Ai-Designer/src/pages/Roomsencepage.jsx
+++ Room-Ai-Designer/src/pages/Roomsencepage.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header.jsx"
 import Roomsence from "../components/Roomsence.jsx";
+import Scaner from "../components/Scaner.jsx";
 
 const Roomsencepage = () => {
     return (
         <>
             <Header />
             <Roomsence />
+            <Scaner/>
         </>
     )
 }
