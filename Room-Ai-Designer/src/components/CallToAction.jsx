97944d50a67861d0a0142d142fff8962ece9d0f2
csrijon <csrijon92@gmail.com>
2025-11-19 13:28:52 +0530

    push

---
+++ Room-Ai-Designer/src/components/CallToAction.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/CallToAction.css";
+
+const CallToAction = () => {
+  return (
+    <div className="cta-wrapper">
+      <h2 className="cta-title">Ready to reimagine your space?</h2>
+      <button className="cta-btn">Start Designing Now</button>
+    </div>
+  );
+};
+
+export default CallToAction;
