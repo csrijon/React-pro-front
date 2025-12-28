80e8808b36342fd68aa6b3d38d8562ac6b4b5076
Srijon Chowdhury <csrijon92@gmail.com>
2025-12-28 16:39:58 +0530

    push

--- new-web/src/components/Fixed.jsx
+++ new-web/src/components/Fixed.jsx
@@ -4,24 +4,40 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import { motion } from "framer-motion"
 import "./Fixed.css"
-import { duration } from '@mui/material/styles';
 
-const aniationvarient ={
-  hidden:{x:50},
+
+const aniationvarient = {
+  hidden: { x: 50 },
+  show: { x: 0, transition: { duration: 0.6 } }
+}
+const secendanimation={
+  hidden:{ x: -50},
   show:{x:0,transition:{duration:0.6}}
 }
 
 const Fixed = () => {
   return (
     <section className='fixed-container' >
-      <motion.div 
-     className="linne"></motion.div>
       <motion.div
-      variants={aniationvarient}
+        className="linne"></motion.div>
+      <motion.div
         className="social-icon">
-        <motion.a initial={{x:-50}} whileInView={{x:0}} href=""><LinkedInIcon /></motion.a>
-        <motion.a  href=""><GitHubIcon /></motion.a>
-        <motion.a initial={{}} href=""><InstagramIcon /></motion.a>
+        <motion.a
+         variants={secendanimation}
+         initial="hidden"
+         whileInView="show"
+        //  whileHover={{rotateX:10}}
+          href=""><LinkedInIcon /></motion.a>
+        <motion.a
+          variants={aniationvarient}
+          initial="hidden"
+          whileInView="show"
+          href=""><GitHubIcon /></motion.a>
+        <motion.a 
+        variants={secendanimation}
+        initial="hidden"
+        whileInView="show"
+        href=""><InstagramIcon /></motion.a>
       </motion.div>
     </section>
   )
