2716587014cdd617380c0d4dd94a990e7fa50ecc
Srijon Chowdhury <csrijon92@gmail.com>
2026-01-31 14:21:47 +0530

    update pages

--- Event-ManagementWebsite/src/ui/Topsection.jsx
+++ Event-ManagementWebsite/src/ui/Topsection.jsx
@@ -3,11 +3,9 @@ import "../css/Topsection.css"
 const Topsection = ({title,number}) => {
     return (
         <section>
-            <div className="container">
             <div className="top-section">
                 <p className="heading" >{title}</p>
                 <p className="view-all" >View All({number})</p>
-            </div>
             </div>
         </section>
     )
