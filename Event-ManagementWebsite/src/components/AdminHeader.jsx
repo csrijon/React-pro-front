0f9a4bf4763c0c3281556f9eb8a42342c3778be6
Srijon Chowdhury <csrijon92@gmail.com>
2026-01-31 19:21:53 +0530

    something wrong

---
+++ Event-ManagementWebsite/src/components/AdminHeader.jsx
@@ -0,0 +1,11 @@
+import "../css/AdminHeader.css"
+
+const AdminHeader = () => {
+    return (
+        <header className="header">
+            <h1>Hello Evano 👋</h1>
+            <input type="text" placeholder="Search" />
+        </header>
+    )
+}
+export default AdminHeader
