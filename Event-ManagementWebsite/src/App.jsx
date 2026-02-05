4b89e0d441146d7ded924ba087dd59c9f5722072
Srijon Chowdhury <csrijon92@gmail.com>
2026-02-06 01:28:29 +0530

    add route name

--- Event-ManagementWebsite/src/App.jsx
+++ Event-ManagementWebsite/src/App.jsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
     element: <><Contactpage /></>
   },
   {
-    path: "/Admin",
+    path: "/admin",
     element: <><AdminLayout /></>,
     children: [
       {
