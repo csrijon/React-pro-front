73728db931f3f7ba5f42660229211706827cc4e7
Srijon Chowdhury <csrijon92@gmail.com>
2026-02-06 01:15:12 +0530

    add all route inmy admin page only has one work wriite logic

--- Event-ManagementWebsite/src/App.jsx
+++ Event-ManagementWebsite/src/App.jsx
@@ -6,6 +6,12 @@ import Mediapage from "./pages/Mediapage.jsx"
 import Contactpage from "./pages/Contactpage.jsx"
 import Sidebar from "./components/Sidebar.jsx";
 import AdminLayout from "./layouts/AdminLayout.jsx";
+import HomeAdmin from "./components/HomeAdmin.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import AboutAdmin from "./components/AboutAdmin.jsx";
+import SuppliersAdmin from "./components/SuppliersAdmin.jsx";
+import MediaAdmin from "./components/MediaAdmin.jsx";
+import ContactAdmin from "./components/ContactAdmin.jsx";
 import { createBrowserRouter } from "react-router"
 import { RouterProvider } from "react-router/dom";
 
@@ -36,8 +42,37 @@ const router = createBrowserRouter([
     element: <><Contactpage /></>
   },
   {
-    path:"/Admin",
-     element:<> <AdminLayout/></>
+    path: "/Admin",
+    element: <><AdminLayout /></>,
+    children: [
+      {
+        index: true,
+        element: <><Dashboard/></>
+      },
+      {
+       path: "home",
+        element: <><HomeAdmin/></>
+      },
+      {
+        path:"supply",
+        element:<><SuppliersAdmin/></>
+      },
+      {
+        path:"aboutadmin",
+        element:<><AboutAdmin/></>
+      },
+      {
+        path:"mediaadmin",
+        element:<><MediaAdmin/></>
+      },
+      {
+        path:"contactadmin",
+        element:<><ContactAdmin/></>
+      }
+    ]
+  },
+  {
+
   }
 ])
 
