6be58ed7128eea77b2b022881f762273728b8ecd
Srijon Chowdhury <csrijon92@gmail.com>
2026-03-17 23:29:27 +0530

    init repo

---
+++ AI-Project-builder/src/components/Herosection.jsx
@@ -0,0 +1,43 @@
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+
+const Herosection = () => {
+    return (
+        <section className="Herosection" >
+            <div className="container">
+
+                <div className="hero-first">
+                    <li>v2.0 NOW LIVE</li>
+                </div>
+                <div className="secend-hero">
+                    <h2>Generate Full-Stacks <span>Projects With Ai</span></h2>
+                </div>
+                <div className="third-hero">
+                    <p>Transform your concepts into production-ready codebases in
+                        seconds. Architecture, authentication, and deployment scripts
+                        included.</p>
+                </div>
+                <div class="input-box">
+                    <div class="input-content">
+                        <span class="icon">💬</span>
+                        <input
+                            type="text"
+                            placeholder="Create a MERN blog app with authentication and dashboard"
+                        />
+                    </div>
+
+                    <button class="generate-btn">
+                        Generate ✨
+                    </button>
+                </div>
+                <div className="vire-button">
+                    <VisibilityIcon/>
+                    <button>View Examples</button>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Herosection
