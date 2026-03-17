74a56d40977d67565d253ca933b87426a6e2a555
Srijon Chowdhury <csrijon92@gmail.com>
2026-03-17 23:29:27 +0530

    init repo

--- AI-Project-builder/src/App.jsx
+++ AI-Project-builder/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header"
+import Herosection from "./components/Herosection"
 import "./App.css"
 
 const App =()=>{
   return(
      <>
-     <Header/></>
+     <Header/><Herosection/></>
   )
 }
  export default App
