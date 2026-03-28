41d3569645800673ccdd6848ce8dc4a192acee05
Srijon Chowdhury <csrijon92@gmail.com>
2026-03-28 17:30:37 +0530

    add

--- Backend-of-Event-Managementwebsite/index.js
+++ Backend-of-Event-Managementwebsite/index.js
@@ -25,7 +25,7 @@ dotenv.config()
 const app = express()
 const port = 3000
 app.use("/uploads", express.static('uploads'));
-app.use(cors({ origin: "https://event-management-gamma-two.vercel.app/" }))
+app.use(cors({ origin: "https://event-management-gamma-two.vercel.app" }))
 app.use(express.json())
 app.use("/api/mailsend", mailsender)
 app.use("/api/fetchtreding", Trandingroute)
