d1961c9bd1e43cc6a48a313fd34cb8203e325ce5
Srijon Chowdhury <csrijon92@gmail.com>
2026-07-17 01:23:51 +0530

    just add fixed section

--- SalonFlow/src/App.jsx
+++ SalonFlow/src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header"
 import Herosection from "./components/Herosection"
 import Mainservicepage from "./components/Mainservicepage"
+import Fixedui from "./ui/Fixedui"
 
 
 const App = ()=>{
   return(
-   <><Header/><Herosection/><Mainservicepage/></>
+   <><Header/><Herosection/><Mainservicepage/><Fixedui/></>
   )
 }
 
---
+++ SalonFlow/src/ui/Fixedui.jsx
@@ -0,0 +1,16 @@
+import "../styles/Fixedui.css"
+
+
+const Fixedui = () => {
+    return (
+        <div className="Fixedui-Section" >
+            <ul>
+                <a href="">Customer</a>
+                <a href="">Provider</a>
+                <a href="">Admin</a>
+            </ul>
+        </div>
+    )
+}
+
+export default Fixedui
